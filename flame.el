28420bd546beb25cf865a50870a95269a501922f
mschuldt <mbschuldt@gmail.com>
2018-03-03 12:12:38 -0800

    require emacs24 for lexical binding

--- flame.el
+++ flame.el
@@ -8,6 +8,7 @@
 ;; Maintainer: Noah Friedman <friedman@splode.com>
 ;; Keywords: games
 ;; Version: 2.0
+;; Package-Requires: ((emacs "24"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
